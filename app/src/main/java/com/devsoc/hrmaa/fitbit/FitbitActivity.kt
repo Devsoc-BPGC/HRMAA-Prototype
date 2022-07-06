4b819df3ada2d87c2970dab01bc10d277d5e5f5e
Yuvraj <mryuvrajsbhadauria@gmail.com>
2022-07-07 02:54:06 +0530

    Feat Added three empty activities PPG, Fitbit & ECG

---
+++ app/src/main/java/com/devsoc/hrmaa/fitbit/FitbitActivity.kt
@@ -0,0 +1,12 @@
+package com.devsoc.hrmaa.fitbit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devsoc.hrmaa.R
+
+class FitbitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fitbit)
+    }
+}
---
+++ app/src/main/java/com/devsoc/hrmaa/ppg/PPGActivity.kt
@@ -0,0 +1,12 @@
+package com.devsoc.hrmaa.ppg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devsoc.hrmaa.R
+
+class PPGActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ppgactivity)
+    }
+}
