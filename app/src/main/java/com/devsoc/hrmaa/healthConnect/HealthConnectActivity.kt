4d47c0b7b8781b26c987739eda10a1e28d1286d2
Yuvraj <mryuvrajsbhadauria@gmail.com>
2022-08-18 12:08:03 +0530

    homescreen ui

---
+++ app/src/main/java/com/devsoc/hrmaa/healthConnect/HealthConnectActivity.kt
@@ -0,0 +1,11 @@
+package com.devsoc.hrmaa.healthConnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthConnectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health_connect)
+    }
+}
