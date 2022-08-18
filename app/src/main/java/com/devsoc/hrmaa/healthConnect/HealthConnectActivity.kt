ba9c6836e6171b7b6344c73e48db9b56a27a8e0e
DS <saraswatdivyansh@gmail.com>
2022-08-18 23:06:50 +0530

    Updated app to use permissions

--- app/src/main/java/com/devsoc/hrmaa/healthConnect/HealthConnectActivity.kt
+++ app/src/main/java/com/devsoc/hrmaa/healthConnect/HealthConnectActivity.kt
@@ -1,12 +1,58 @@
 package com.devsoc.hrmaa.healthConnect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.permission.Permission
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.StepsRecord
+import androidx.lifecycle.lifecycleScope
 import com.devsoc.hrmaa.R
+import kotlinx.coroutines.launch
 
 class HealthConnectActivity : AppCompatActivity() {
+    // Create the permissions launcher.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health_connect)
+
+        if (HealthConnectClient.isAvailable(this)) {
+            // Health Connect is available and installed.
+            val healthConnectClient = HealthConnectClient.getOrCreate(this)
+            val requestPermissionActivityContract =
+                healthConnectClient.permissionController.createRequestPermissionActivityContract()
+
+            val requestPermissions =
+                registerForActivityResult(requestPermissionActivityContract) { granted ->
+                    if (granted.containsAll(PERMISSIONS)) {
+                        // Permissions successfully granted
+                    } else {
+                        // Lack of required permissions
+                    }
+                }
+            fun checkPermissionsAndRun(client: HealthConnectClient) {
+                lifecycleScope.launch {
+                    val granted = client.permissionController.getGrantedPermissions(PERMISSIONS)
+                    if (granted.containsAll(PERMISSIONS)) {
+                        // Permissions already granted
+                    } else {
+                        requestPermissions.launch(PERMISSIONS)
+                    }
+                }
+            }
+
+        } else {
+            // ...
+        }
     }
+
+
+    val PERMISSIONS =
+        setOf(
+            Permission.createReadPermission(HeartRateRecord::class),
+            Permission.createWritePermission(HeartRateRecord::class),
+            Permission.createReadPermission(StepsRecord::class),
+            Permission.createWritePermission(StepsRecord::class)
+        )
 }
