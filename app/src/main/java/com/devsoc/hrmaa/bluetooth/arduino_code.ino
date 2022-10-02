4d0b5196360d60258ef4a6cc3cf2ae368c1ed39d
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-10-02 11:20:48 +0530

    Real sensor Sanaya ecg dil sensor integrated

--- app/src/main/java/com/devsoc/hrmaa/bluetooth/arduino_code.ino
+++ app/src/main/java/com/devsoc/hrmaa/bluetooth/arduino_code.ino
@@ -1,36 +1,42 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTserial(10, 11); // RX | TX
-int sensorPin = A0;
-int sensorValue = 0;
-int fuckUpLED=12;
-int allIsWellLED=2;
-int waitingLED=3;
+
+SoftwareSerial BTserial(2, 3); // RX | TX
+int waitingLED=4;
+
+//Sayani_ka_ecg
+int LONeg = 10;
+int LOPos = 11;
+int ECGRead = A0;
 
 void setup() {
+
   BTserial.begin(9600);
   Serial.begin(9600);
-  }
+
+  pinMode( LONeg, INPUT);
+  pinMode( LOPos, INPUT);
+
+}
 
 void loop() {
+
   digitalWrite(waitingLED,HIGH);
   //necesssary to wait else, the arduino will keep on taking in values and put it in the
   //output buffer. From where they will flood into the android
   //on connection
   while(BTserial.available() == 0){
     Serial.println("Waiting for serial");
+    BTserial.flush();
       }
+  //BTserial.read() on input 1 wont give 1, it will give 210
   digitalWrite(waitingLED,LOW);
-//  int btSerialStuff =BTserial.read();
-//  Serial.print(btSerialStuff);
-//    if(btSerialStuff==210){
-//      digitalWrite(allIsWellLED,HIGH);
-//    }
-//    else{
-//      digitalWrite(fuckUpLED,HIGH);
-//      while(true){}
-//    }
-  sensorValue = analogRead(sensorPin);
-  BTserial.println(sensorValue);
-  Serial.println(sensorValue);
+
+  if((digitalRead(LONeg) == 1) || (digitalRead(LOPos) == 1)){
+    BTserial.println("!");
+  }
+  else{
+    BTserial.println(analogRead(ECGRead));
+  }
   delay(1000);
+
 }
