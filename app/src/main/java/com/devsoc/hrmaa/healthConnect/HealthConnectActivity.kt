5d095caf08f52cd23d2ca43f6a3b299f2715af63
AdiKul1294 <f20211294@goa.bits-pilani.ac.in>
2023-01-09 18:24:04 +0530

    added reading heartrate data functionality

--- app/src/main/java/com/devsoc/hrmaa/healthConnect/HealthConnectActivity.kt
+++ app/src/main/java/com/devsoc/hrmaa/healthConnect/HealthConnectActivity.kt
@@ -2,19 +2,25 @@ package com.devsoc.hrmaa.healthConnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.registerForActivityResult
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.permission.Permission
 import androidx.health.connect.client.records.HeartRateRecord
 import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
 import androidx.lifecycle.lifecycleScope
-import com.devsoc.hrmaa.R
+import com.devsoc.hrmaa.databinding.ActivityHealthConnectBinding
 import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.ZonedDateTime
 
 class HealthConnectActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHealthConnectBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_health_connect)
+        binding = ActivityHealthConnectBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         if (HealthConnectClient.isAvailable(this)) {
             // Health Connect is available and installed.
@@ -23,12 +29,8 @@ class HealthConnectActivity : AppCompatActivity() {
             val requestPermissionActivityContract = healthConnectClient.permissionController.createRequestPermissionActivityContract()
 
             val requestPermissions =
-                registerForActivityResult(requestPermissionActivityContract) { granted ->
-                    if (granted.containsAll(PERMISSIONS)) {
-                        // Permissions successfully granted
-                    } else {
-                        // Lack of required permissions
-                    }
+                registerForActivityResult(requestPermissionActivityContract) {
+
                 }
             // Create the permissions launcher.
 
@@ -37,17 +39,49 @@ class HealthConnectActivity : AppCompatActivity() {
                     val granted = client.permissionController.getGrantedPermissions(PERMISSIONS)
                     if (granted.containsAll(PERMISSIONS)) {
                         // Permissions already granted
+                        val end = Instant.now()
+                        val start = ZonedDateTime.now().minusYears(10).toInstant()
+                        readHeartRateByTimeRange(healthConnectClient, start, end)
                     } else {
                         requestPermissions.launch(PERMISSIONS)
                     }
                 }
             }
+
+            checkPermissionsAndRun(healthConnectClient)
         } else {
             // ...
         }
 
+    }
 
+    fun readHeartRateByTimeRange(
+        healthConnectClient: HealthConnectClient,
+        startTime: Instant,
+        endTime: Instant
+    ) {
+        lifecycleScope.launch {
+            val response =
+                healthConnectClient.readRecords(
+                    ReadRecordsRequest(
+                        HeartRateRecord::class,
+                        timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                    )
+                )
 
+            if(response.records.isNotEmpty()) {
+                for (rec in response.records) {
+                    //TODO : process each heart rate record
+                    for(hr in rec.samples){
+
+                    }
+                }
+            }
+            else {
+                binding.dataTvHca.text = "No records found!"
+            }
+
+        }
     }
 
     val PERMISSIONS =
